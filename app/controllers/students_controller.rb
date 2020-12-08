efccffc487d0181ba4f7f8bd92066a6c0ebf7aab
Kelin Field <kfield111@hotmail.com>
2020-12-07 20:31:35 -0600

    Lab updates 1

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,28 @@
+class StudentsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        @student = Student.new(student_params)
+        @student.save
+        redirect_to student_path(@student)
+    end
+
+    def show
+    end
+
+    def update
+            @student = Student.find(params[:id])
+            @student.update(student_params)
+            redirect_to student_path(@student)
+    end
+
+    private
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name)
+    end
+
+
+end
